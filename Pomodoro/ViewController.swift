994fd37b5528ada518a1a371f998ef6c58885a64
Sonnie Hiles <sonniehiles@Bruce.local>
2019-01-13 15:36:48 -0600

    Add basic countdown timer

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let work: Int =  25
+    
+    @IBOutlet weak var timeTicker: UITextField!
+    @IBOutlet weak var playPauseButton: UIButton!
+    
+    var timer: Timer = Timer()
+    var timeRemaining: Int = 0
+    var timing = false
+    
+    @IBAction func playPause(_ sender: Any) {
+        if(!timing){
+            timing = true
+            playPauseButton.setTitle("Pause", for: .normal)
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
+        } else {
+            timing = false
+            playPauseButton.setTitle("Start", for: .normal)
+            timer.invalidate()
+        }
+    }
+    
+    @objc func updateTimer() {
+        timeRemaining -= 1
+        timeTicker.text = String(timeRemaining)
+        if(timeRemaining == 0){
+            timer.invalidate()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        timeRemaining = work
     }
-
-
 }
-
