b532386b1b555a57ce3eef15d3203d9a98c43c39
Sonnie Hiles <sonniehiles@Bruce.local>
2019-01-13 10:49:14 -0600

    Initial Commit

---
+++ Pomodoro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by Sonnie Hiles on 13/01/2019.
+//  Copyright © 2019 Sonnie Hiles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
