868ebe727d96feb210a7b7537367e5d8ca5e4e71
Sonnie Hiles <psysh2@nottingham.ac.uk>
2019-03-04 18:17:53 +0000

    Add UI tests for timer view controller

--- PomodoroUITests/PomodoroUITests.swift
+++ PomodoroUITests/PomodoroUITests.swift
@@ -11,24 +11,40 @@ import XCTest
 class PomodoroUITests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSkipButtonEnablesWhenStartingTimer() {
+        //Start Timer
+        let app = XCUIApplication()
+        app.buttons["START"].tap()
+        app.sheets["Select Subject for Session"].buttons["English"].tap()
+        //Verify skip Button is enabled
+        XCTAssertTrue(app.buttons["Skip"].isEnabled)
     }
-
+    
+    func testSkipButtonDisablesWhenResetingTimer(){
+        //Reset Timer
+        let app = XCUIApplication()
+        app.buttons["START"].tap()
+        app.sheets["Select Subject for Session"].buttons["English"].tap()
+        app.buttons["RESET"].tap()
+        
+        //Verify skip Button is disabled
+        XCTAssertFalse(app.buttons["Skip"].isEnabled)
+    }
+    
+    func testResetButtonShowsWhenStartingTimer() {
+        //Start Timer
+        let app = XCUIApplication()
+        app.buttons["START"].tap()
+        app.sheets["Select Subject for Session"].buttons["English"].tap()
+        //Verify Reset button isn't hidden
+        XCTAssertTrue(app.buttons["RESET"].isEnabled)
+    }
 }
