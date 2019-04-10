17f5c5c6a763a1088e765b244e97452292b6bf91
Sonnie Hiles <psysh2@nottingham.ac.uk>
2019-04-10 07:42:09 +0300

    Add alerts and handling for persistence storage errors

--- Pomodoro/AppDelegate.swift
+++ Pomodoro/AppDelegate.swift
@@ -65,44 +65,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-         */
+
         let container = NSPersistentContainer(name: "pomodoro")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let errorAlert = UIAlertController(title:"Error opening storage, can't open", message: error.userInfo.description, preferredStyle: .alert)
+                errorAlert.addAction(UIAlertAction(title: "Close", style: .cancel, handler: nil))
                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+                let window = UIWindow(frame: UIScreen.main.bounds)
+                let main = MainTabbedViewController()
+                window.rootViewController = main
+                window.makeKeyAndVisible()
+                main.present(errorAlert, animated: true, completion: nil)
+                
+                fatalError("Error opening pomodoro container: error \(error), \(error.userInfo)")
             }
         })
         return container
     }()
     
-    // MARK: - Core Data Saving support
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
             do {
                 try context.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                showAlert(title: "Error saving data", reason: nserror.userInfo.description)
             }
         }
+    }
+    
+    func showAlert(title: String, reason: String) {
+        let error = UIAlertController(title: title, message: reason, preferredStyle: .alert)
+        error.addAction(UIAlertAction(title: "Close", style: .cancel, handler: nil))
+        main?.present(error, animated: true, completion: nil)
     }
 }
